c702714362f63bf23a0ce3c54ebb7a029dbac2b9
grey-abu <fahrihasanabu@gmail.com>
2025-04-29 09:53:16 +0700

    Add files via upload

---
+++ rumah.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Daftar</title>
+    <style>
+        body {
+  font-family: Arial, sans-serif;
+  background-image: url(714746.jpg);
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+}
+
+.form-box {
+  background-color: rgba(255, 255, 255, 0.418); 
+  padding: 100px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  width: 330px;
+  height: 10%;
+  text-align: center;
+  backdrop-filter: blur(20px); 
+}
+
+form {
+  margin-top: -20px;
+}
+
+h3 {
+    margin-top: -20px;
+    font-size: 35px;
+}
+
+h4 {
+    margin-top: -20px;
+    font-size: 30px;
+}
+
+a {
+    background: #fff;
+    font-size: 20px;
+    text-decoration: none;
+    border: 4px solid black;
+    border-radius: 4px solid;
+}
+
+input {
+  width: 90%;
+  padding: 10px;
+  margin: 10px 0;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  background-color: #0078f8;
+  border: none;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.msg {
+  margin-top: 10px;
+  color: red;
+}
+
+    </style>
+</head>
+<body>
+    <div class="form-box">
+       
+        <form method="POST">
+            <h3>SELAMAT DATANG</h3>
+            <h4>DI SISTEM LOGIN</h4>
+            <a href="login.php">Login</a>
+        </form>
+    </div>
+</body>
+</html>
